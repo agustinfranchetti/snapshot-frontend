aea6433575657897f54544f3d76b905cf0cf7cac
agustinfranchetti <agustinfranchetti@gmail.com>
2022-10-27 17:21:53 -0300

    removed network switch atm

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,25 +10,20 @@ import {
 import { Button, Flex, Text } from "@chakra-ui/react";
 
 export const Login = () => {
-  const { address, isConnected } = useAccount();
-  const { data: ensName } = useEnsName({
-    // uncomment to test with ENS, this is vitalik.eth's address
-    // address: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
-    address,
-    chainId: 1,
-  });
+  const { isConnected } = useAccount();
   const { chain: network } = useNetwork();
   const { disconnect } = useDisconnect();
   const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect();
   const networkSwitch = useSwitchNetwork();
 
-  if (isLoading && network?.id !== 137) {
-    if (networkSwitch?.switchNetwork) {
-      //call only once to avoid infinite loop
-      networkSwitch.switchNetwork(137);
-    }
-  }
+  //TODO: add a way to switch networks
+  // if (isLoading && network?.id !== 137) {
+  //   if (networkSwitch?.switchNetwork) {
+  //     //call only once to avoid infinite loop
+  //     networkSwitch.switchNetwork(137);
+  //   }
+  // }
 
   return !isConnected ? (
     <Flex
