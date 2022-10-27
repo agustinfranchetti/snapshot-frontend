a3cc7e754ca096c3a4d356c5dab260fec76e824f
agustinfranchetti <agustinfranchetti@gmail.com>
2022-10-27 17:53:57 -0300

    added secondary image to frind post

--- src/components/MainScreen/FriendsPosts/FriendsPosts.tsx
+++ src/components/MainScreen/FriendsPosts/FriendsPosts.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import { FriendPostHeader } from "./FriendPostHeader";
 
@@ -7,16 +8,46 @@ import { FriendPostHeader } from "./FriendPostHeader";
  * @returns {JSX.Element}
  */
 export const FriendsPosts = ({ randomImageSig }) => {
+  const backCameraImage = "https://source.unsplash.com/user/c_v_r/300x200";
+  const frontCameraImage =
+    "https://source.unsplash.com/user/samuelzeller/300x200";
+
+  const [mainImage, setMainImage] = useState(backCameraImage);
+  const [secondaryImage, setSecondaryImage] = useState(frontCameraImage);
+
+  const handleImageChange = () => {
+    if (mainImage === backCameraImage) {
+      setMainImage(frontCameraImage);
+      setSecondaryImage(backCameraImage);
+    } else {
+      setMainImage(backCameraImage);
+      setSecondaryImage(frontCameraImage);
+    }
+  };
+
   return (
     <Box display={"flex"} flexDirection={"column"} textAlign="left" mb={10}>
       <FriendPostHeader />
-      <Image
-        src={`https://source.unsplash.com/user/c_v_r/300x200?sig=${randomImageSig}`}
-        alt="sample_image"
-        borderRadius="15"
+      <Box
+        key="mainImage"
+        backgroundImage={mainImage}
+        borderRadius="10"
         boxSize="65vh"
         w={"90vw"}
-      />
+        backgroundSize="cover"
+      >
+        <Image
+          src={secondaryImage}
+          alt="secondaryImage"
+          borderRadius="10"
+          boxSize="20vh"
+          w={"30vw"}
+          style={{ objectFit: "cover" }}
+          onClick={handleImageChange}
+          mt={2}
+          ml={2}
+        />
+      </Box>
       <Text color={"gray.500"}>Add a comment...</Text>
     </Box>
   );
